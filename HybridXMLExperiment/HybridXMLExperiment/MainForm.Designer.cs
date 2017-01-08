addc6b54aee9c5da04bfd0b527c9258162a2f6ea
Joshua Prickett <drjoshuaprickett@gmail.com>
2017-01-08 11:02:43 -0700

    Test

--- HybridXMLExperiment/HybridXMLExperiment/MainForm.Designer.cs
+++ HybridXMLExperiment/HybridXMLExperiment/MainForm.Designer.cs
@@ -33,14 +33,14 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.txtLiveOutput = new System.Windows.Forms.TextBox();
+            this.chartOutData = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.gridDataStream = new System.Windows.Forms.DataGridView();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtLiveOutput = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartOutData)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridDataStream)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -55,7 +55,7 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.chart1);
+            this.tabPage1.Controls.Add(this.chartOutData);
             this.tabPage1.Controls.Add(this.gridDataStream);
             this.tabPage1.Controls.Add(this.txtLiveOutput);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
@@ -66,23 +66,21 @@
             this.tabPage1.Text = "Viewer";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // chartOutData
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1163, 500);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Configuration";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // txtLiveOutput
-            // 
-            this.txtLiveOutput.Location = new System.Drawing.Point(3, 37);
-            this.txtLiveOutput.Multiline = true;
-            this.txtLiveOutput.Name = "txtLiveOutput";
-            this.txtLiveOutput.Size = new System.Drawing.Size(1154, 169);
-            this.txtLiveOutput.TabIndex = 0;
+            chartArea1.Name = "ChartArea1";
+            this.chartOutData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartOutData.Legends.Add(legend1);
+            this.chartOutData.Location = new System.Drawing.Point(636, 212);
+            this.chartOutData.Name = "chartOutData";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartOutData.Series.Add(series1);
+            this.chartOutData.Size = new System.Drawing.Size(514, 150);
+            this.chartOutData.TabIndex = 2;
+            this.chartOutData.Text = "chart1";
             // 
             // gridDataStream
             // 
@@ -92,21 +90,23 @@
             this.gridDataStream.Size = new System.Drawing.Size(624, 150);
             this.gridDataStream.TabIndex = 1;
             // 
-            // chart1
+            // txtLiveOutput
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(636, 212);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(514, 150);
-            this.chart1.TabIndex = 2;
-            this.chart1.Text = "chart1";
+            this.txtLiveOutput.Location = new System.Drawing.Point(3, 37);
+            this.txtLiveOutput.Multiline = true;
+            this.txtLiveOutput.Name = "txtLiveOutput";
+            this.txtLiveOutput.Size = new System.Drawing.Size(1154, 169);
+            this.txtLiveOutput.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1163, 500);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Configuration";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -119,8 +119,8 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartOutData)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridDataStream)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -130,7 +130,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartOutData;
         private System.Windows.Forms.DataGridView gridDataStream;
         private System.Windows.Forms.TextBox txtLiveOutput;
     }
